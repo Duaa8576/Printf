dc1b279f093c4c8d6bb8cd93537e26c42db0782d
Marwa-Elhag <marwaalhaj89@gmail.com>
2023-10-16 14:00:18 -0700

    Update

---
+++ parameter.c
@@ -0,0 +1,24 @@
+#include <main.h>
+/**
+ * init_params -  resets buffer and clear field
+ * @pr: parameter
+ * @ap: pointer
+ * return: success
+ */
+
+void init_params(pr_t *pr, va_list ap);
+{
+	pr->uni = 0;
+	pr->zero = 0;
+	pr->plus = 0;
+	pr->minus = 0;
+	pr->space = 0;
+	pr->hash = 0;
+
+	pr->h = 0;
+	pr->l = 0;
+
+	pr->width = 0;
+	pr->preci = UINT_MAX;
+	(void)ap;
+}
