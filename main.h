79df78b80adda75cc1ee1378a27130b326695e3b
Duaa8576 <duaaaaa51@gmail.com>
2023-10-18 02:05:50 +0300

    hi

--- main.h
+++ main.h
@@ -102,5 +102,4 @@ int print_number_left_shift(char *str, pr_t *pr);
 void init_params(pr_t *pr, va_list ap);
 char get_precision(char *p, pr_t *pr, va_list ap);
 int _printf(const char *format, ...);
-
 #endif
