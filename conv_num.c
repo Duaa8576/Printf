c38225a09ccd49dd68187a72bfd95c6d29d9377d
Duaa8576 <duaaaaa51@gmail.com>
2023-10-18 00:53:52 +0300

    hi

--- conv_num.c
+++ conv_num.c
@@ -1,34 +1,107 @@
 #include "main.h"
 
 /**
- * prit_hex - hex lower
+ * print_hex - hex lower
  * @ap: pointer
  * @pr: struct
  * Return: Success
 */
-int prit_hex(va_list ap, pr_t *pr)
+int print_hex(va_list ap, pr_t *pr)
+{
+        unsigned long l;
+        int n = 0;
+        char *str;
+
+        if (pr->l)
+                l = (unsigned long)va_arg(ap, unsigned long);
+        else if (pr->h)
+                l = (unsigned short int)va_arg(ap, unsigned int);
+        else
+                l = (unsigned int)va_arg(ap, unsigned int);
+
+        str = convert(l, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, pr);
+        if (pr->hash && l)
+        {
+                *--str = 'x';
+                *--str = '0';
+        }
+        pr->uni = 1;
+        return (n += print_number(str, pr));
+
+}
+
+/**
+ * print_HEX - hexadicimal upper
+ * @ap: pointer
+ * @pr: struct
+ * Return: Success
+*/
+
+{
+        unsigned long l;
+        int n = 0;
+        char *str;
+
+        if (pr->l)
+                l = (unsigned long)va_arg(ap, unsigned long);
+        else if (pr->h)
+                l = (unsigned short int)va_arg(ap, unsigned int);
+        else
+                l = (unsigned int)va_arg(ap, unsigned int);
+
+        str = convert(l, 16, CONVERT_UNSIGNED, pr);
+        if (pr->hash && l)
+        {
+                *--str = 'X';
+                *--str = '0';
+        }
+        pr->uni = 1;
+        return (n += print_number(str, pr));
+
+}
+
+/**
+ * print_binary - binary number
+ * @ap: pointer
+ * @pr: struct
+ * Return: Success
+*/
+
+int print_binary(va_list ap, pr_t *pr)
+{
+	unsigned int n = va_arg(ap, unsigned int);
+	char *str = convert(n, 2, CONVERT_UNSIGNED, pr);
+	int c = 0;
+
+	if (pa->hash && n)
+		*--str = '0';
+	pr->uni = 1;
+	return (c += print_number(str, pr));
+}
+
+/**
+ * print_octal - octal number
+ * @ap: pointer
+ * @pr: struct
+ * Return: Success
+*/
+
+int print_octal(va_list ap, pr_t *pr)
 {
 	unsigned long l;
-	int n = 0;
 	char *str;
+	int c = 0;
 
-	if (pr->l_modifier)
-		l = (unsigned long)va_arg(ap, unsigned long);
-	else if (pr->h_modifier)
+	if (pr->l)
+		l= (unsigned long)va_arg(ap, unsigned long);
+	else if (pr->h)
 		l = (unsigned short int)va_arg(ap, unsigned int);
 	else
 		l = (unsigned int)va_arg(ap, unsigned int);
+	str = convert(1, 8, CONVERT_UNSIGNED, pa);
 
-	str = convert(l, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, pr);
-	if (pr->hash && l)
-	{
-		*--str = 'x';
+	if (pa->hash && l)
 		*--str = '0';
-	}
-	pr->unsign = 1;
-	return (n += print_number(str, pr));
-
+	pr->uni = 1;
+	return (c += print_number(str, pr));
 }
-int prit_binary(va_list ap, pr_t *pr)
-int prit_octal(va_list ap, pr_t *pr)
-int prit_HEX(va_list ap, pr_t *pr)
